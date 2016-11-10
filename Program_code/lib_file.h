d65b710f705b890ec7f3f06328fdf37303500b7b
Eric <eric@eric-VirtualBox.(none)>
2016-11-10 16:20:48 +0800

    Add include lib_file.h(include all needed liberary)

---
+++ Program_code/lib_file.h
@@ -0,0 +1,13 @@
+
+/*Basic lib*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*time lib*/
+#include <time.h>
+#include <unistd.h>
+
+/*Variable type*/
+#include <string.h>
+#include <sys/types.h>
+#include <stdint.h>
