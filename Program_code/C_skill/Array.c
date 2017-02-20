8c7362b914f5ad29b1d6c22c575fd9c76b768c99
surface-ericchen <a268591423@gmail.com>
2017-02-20 09:29:33 +0800

    add some skill in Array.c

--- Program_code/C_skill/Array.c
+++ Program_code/C_skill/Array.c
@@ -41,9 +41,102 @@ Answer : 2004 2020   // Name of array in C gives the address(expect in sizeof op
 					// Applying the address-of operator before the array name gives the address of the who-
 					//	-le array. Adding 1 to this address gives the address plus the sizeof whole array.
 
+/*********************************************************************************/
+
+
+/********************************Size of pass array to function ***********************************/
+void print(int arr[]) // if pass for function array, it needed also to pass array size.
+{
+	int n = sizeof(arr)/sizeof(arr[0]);  --> // 32bit : sizeof(arr) -> 4  ; 64bit : sizeof(arr) -> 8
+											// sizeof(arr)/sizeof(arr[0]) becomes sizeof(int *)/sizeof(int)
+	int i;
+	for(i = 0;i<n;i++)
+		printf("%d",arr[i]);
+}
+int main()
+{
+	int arr[] = {1,2,3,4,5};
+	print(arr);
+	return 0;
+}
+
+Answer : 1 
+
+/***************************************************************************************************/
 
 
 
+/******************************** Array addres plus 1***********************************/
+
+int main()
+{
+	int as[] = {1,2,3,4,5,6};
+	int *ptr = (int *)(&as+1);  --> // It's gets 'as' array all values 1 ~ 6 ,and also gets next value 7;
+	printf("%d", *(ptr-1));  --> // It's show ,that address 7 div 1 . So back to the address 6.
+								//But if *(ptr) for show , it's overflow .
+	return 0;
+}
+
+Answer : 6
+
+/***************************************************************************************************/
+
+
+/*************************** Pointer Array ***************************************************/
+int *A[10];
+int C[] = {12,13,14,15};
+
+A[2] = C;
+
+Answer : A[2][3] = 15;  A[2][0] = 12;
+
+A[0] = C;
+
+Answer : A[0][3] = 15; A[0][0] = 12;
 
 
 
+/***************************************************************************************************/
+
+
+
+/**************************  Array ***************************************************/
+
+For every type T expect void and function type, there can be an array of T.
+
+
+//For Pointer aray
+int main()
+{
+	void arr[100] --> //It's compiler error
+}
+
+int main()
+{
+	void *arr[100] --> //We can have array of void pointers and function pointers.
+}
+
+//Function pointer in c 
+
+void fun(int a)
+{
+	printf("Value of a is %d\n",a);
+}
+
+int main()
+{
+	void (*fun_ptr)(int) = &fun; -->//fun_ptr is pointer to function fun();
+	
+	/*
+		The above line is equivalent of following two
+		void (*fun_ptr)(int);
+		fun_ptr = &fun;
+	*/
+
+
+	(*fun_ptr)(10); --> //Invoking fun() using fun_ptr
+}
+
+Answer : Value of 1 is 10
+
+/***************************************************************************************************/
